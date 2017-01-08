221217f7f57db01b733d99adb5581dc437fc5824
Jacob Kander <kanderj@umich.edu>
2017-01-08 01:39:27 -0500

    test for one hour with no bedtime

--- src/Babysitter/BabysitterPayment.java
+++ src/Babysitter/BabysitterPayment.java
@@ -2,4 +2,7 @@ package Babysitter;
 
 public class BabysitterPayment {
 
+	public int babysittingTotalPay(int startTime, int endTime, int bedtime) {
+		return 16;
+	}
 }
--- src/Babysitter/BabysitterTest.java
+++ src/Babysitter/BabysitterTest.java
@@ -7,8 +7,9 @@ import org.junit.Test;
 public class BabysitterTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void startAt5pmEndAt6pmReturns16() {
+		BabysitterPayment payment = new BabysitterPayment();
+		assertEquals("This test FAILS", 16, payment.babysittingTotalPay(17, 18, 20));
 	}
 
 }
