ddb6f922f9174a52eeebd2a5334ba1797c8ea4c2
Jacob Kander <kanderj@umich.edu>
2017-01-09 15:43:53 -0500

    Refactor to make rates variables

--- src/Babysitter/BabysitterPayment.java
+++ src/Babysitter/BabysitterPayment.java
@@ -3,6 +3,9 @@ package Babysitter;
 public class BabysitterPayment {
 
 	int paymentAmount = 0;
+	int rate1 = 12;
+	int rate2 = 8; 
+	int rate3 = 16;
 
 	public int babysittingTotalPay(int startTime, int endTime, int bedtime) {
 
@@ -11,16 +14,16 @@ public class BabysitterPayment {
 		}
 			
 		if (bedtime > endTime) {
-			paymentAmount += (endTime - startTime) * 12;
+			paymentAmount += (endTime - startTime) * rate1;
 
 		} else if (endTime <= 23) {
-			paymentAmount += (bedtime - startTime) * 12;
-			paymentAmount += (endTime - bedtime) * 8;
+			paymentAmount += (bedtime - startTime) * rate1;
+			paymentAmount += (endTime - bedtime) * rate2;
 
 		} else {
-			paymentAmount += (bedtime - startTime) * 12;
-			paymentAmount += (24 - bedtime) * 8;  
-			paymentAmount += (endTime - 24) * 16; 
+			paymentAmount += (bedtime - startTime) * rate1;
+			paymentAmount += (24 - bedtime) * rate2;  
+			paymentAmount += (endTime - 24) * rate3; 
 		}
 
 		return paymentAmount;
